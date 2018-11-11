0c325a2c3afef1f5699293cb1c2fd4f9869b857f
lpuigo <laurent.puig@gmail.com>
2018-11-11 22:52:39 +0100

    website : refactored worksite model 2

--- model/worksite.go
+++ model/worksite.go
@@ -11,3 +11,7 @@ type Worksite struct {
 func MakeWorksite(ref, orderdate string, pmz, pa PT, order ...Order) Worksite {
 	return Worksite{Ref: ref, Pmz: pmz, Pa: pa, Orders: order, OrderDate: orderdate}
 }
+
+func (w Worksite) FileName() string {
+	return w.OrderDate + "_" + w.Ref
+}
