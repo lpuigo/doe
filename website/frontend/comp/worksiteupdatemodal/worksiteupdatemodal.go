c0c1d28e0e60f08e69213cf42b6d79242d82aebc
lpuigo <laurent.puig@gmail.com>
2019-01-17 23:36:24 +0100

    FE : Update Worksite Modal WIP

--- website/frontend/comp/worksiteupdatemodal/worksiteupdatemodal.go
+++ website/frontend/comp/worksiteupdatemodal/worksiteupdatemodal.go
@@ -5,14 +5,18 @@ import (
 	"github.com/huckridgesw/hvue"
 	wem "github.com/lpuig/ewin/doe/website/frontend/comp/worksiteeditmodal"
 	"github.com/lpuig/ewin/doe/website/frontend/comp/worksiteinfo"
+	fm "github.com/lpuig/ewin/doe/website/frontend/model"
 )
 
 type WorksiteUpdateModalModel struct {
 	*wem.WorksiteEditModalModel
+
+	Filter string `js:"filter"`
 }
 
 func NewWorksiteUpdateModalModel(vm *hvue.VM) *WorksiteUpdateModalModel {
 	wumm := &WorksiteUpdateModalModel{WorksiteEditModalModel: wem.NewWorksiteEditModalModel(vm)}
+	wumm.Filter = ""
 	return wumm
 }
 
@@ -43,12 +47,12 @@ func ComponentOptions() []hvue.ComponentOption {
 		}),
 		hvue.MethodsOf(&WorksiteUpdateModalModel{}),
 
-		//hvue.Computed("isNewWorksite", func(vm *hvue.VM) interface{} {
-		//	m := &WorksiteEditModalModel{Object: vm.Object}
-		//	return m.CurrentWorksite.Id == -1
-		//}),
+		hvue.Computed("filteredTroncons", func(vm *hvue.VM) interface{} {
+			m := NewWorksiteUpdateModalModelFromJS(vm.Object)
+			return m.GetTroncons()
+		}),
 		hvue.Computed("hasChanged", func(vm *hvue.VM) interface{} {
-			m := NewWorksiteUpdateModalModel(vm)
+			m := NewWorksiteUpdateModalModelFromJS(vm.Object)
 			return m.HasChanged()
 		}),
 		hvue.Computed("hasWarning", func(vm *hvue.VM) interface{} {
@@ -58,8 +62,42 @@ func ComponentOptions() []hvue.ComponentOption {
 			//}
 			return "success"
 		}),
+		hvue.Filter("FormatTronconRef", func(vm *hvue.VM, value *js.Object, args ...*js.Object) interface{} {
+			m := NewWorksiteUpdateModalModelFromJS(vm.Object)
+			t := &fm.Troncon{Object: value}
+			return m.GetFormatTronconRef(t)
+		}),
+		hvue.Filter("FormatStatus", func(vm *hvue.VM, value *js.Object, args ...*js.Object) interface{} {
+			m := NewWorksiteUpdateModalModelFromJS(vm.Object)
+			t := &fm.Troncon{Object: value}
+			return m.GetFormatStatus(t)
+		}),
 	}
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////////
 // Modal Methods
+
+func (wumm *WorksiteUpdateModalModel) GetTroncons() []*fm.Troncon {
+	res := []*fm.Troncon{}
+	for _, o := range wumm.CurrentWorksite.Orders {
+		for _, t := range o.Troncons {
+			res = append(res, t)
+		}
+	}
+
+	return res
+}
+
+func (wumm *WorksiteUpdateModalModel) TableRowClassName(rowInfo *js.Object) string {
+	//wsi := &fm.WorksiteInfo{Object: rowInfo.Get("row")}
+	return ""
+}
+
+func (wumm *WorksiteUpdateModalModel) GetFormatTronconRef(t *fm.Troncon) string {
+	return t.Ref
+}
+
+func (wumm *WorksiteUpdateModalModel) GetFormatStatus(t *fm.Troncon) string {
+	return "TODO"
+}
--- website/frontend/comp/worksiteupdatemodal/worksiteupdatemodal_tmpl.go
+++ website/frontend/comp/worksiteupdatemodal/worksiteupdatemodal_tmpl.go
@@ -25,10 +25,59 @@ const template string = `
 	<!-- 
 		Modal Body
 	-->
-	<div v-loading="loading" style="min-height: 35vh;max-height: 65vh;overflow-x: hidden;overflow-y: auto;padding-right: 6px;">
-		<pre>
-            {{current_worksite}}
-        </pre>
+	<div v-loading="loading" style="min-height: 35vh;height: 65vh;overflow-x: hidden;overflow-y: auto;padding-right: 6px;">
+        <el-container style="height: 100%">
+            <el-header style="height: auto; padding: 5px">
+                <el-row :span="20">
+                    <el-col :offset="20" :span="4">
+                        <el-input
+                                placeholder="filtre"
+                                prefix-icon="el-icon-search"
+                                v-model="filter"
+                                size="mini"	clearable
+                        ></el-input>
+                    </el-col>
+                </el-row>
+            </el-header>
+            <el-main  style="height: 100%; padding: 0px">
+                <el-table
+                        :data="filteredTroncons"
+                        :row-class-name="TableRowClassName"
+                        height="100%" :border=true size="mini"
+                >
+                    <el-table-column
+                            label="Tronçon"
+                            :resizable="true" :show-overflow-tooltip=true
+                    >
+                        <tempalte slot-scope="scope">
+                            <span>{{scope.row | FormatTronconRef}}</span>
+                        </tempalte>
+                    </el-table-column>
+                    <el-table-column
+                            label="Status"
+                            :resizable="true" :show-overflow-tooltip=true
+                    >
+                        <tempalte slot-scope="scope">
+                            <span>{{scope.row | FormatStatus}}</span>
+                        </tempalte>
+                    </el-table-column>
+                    <el-table-column
+                            label="Installation"
+                            :resizable="true"
+                    >
+                        <tempalte slot-scope="scope">
+                            <el-date-picker format="dd/MM/yyyy" placeholder="Date Install." size="mini"
+                                            style="width: 100%" type="date"
+                                            v-model="scope.row.InstallDate"
+                                            value-format="yyyy-MM-dd"
+                                            :picker-options="{firstDayOfWeek:1, disabledDate(time) { return time.getTime() > Date.now(); }}"
+                                            :clearable="false"
+                            ></el-date-picker>
+                        </tempalte>
+                    </el-table-column>
+                </el-table>
+            </el-main>
+        </el-container>
 	</div>
 
 	<!-- 
